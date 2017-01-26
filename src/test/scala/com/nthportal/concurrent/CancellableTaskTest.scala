d76f557055faec387761094ed15e610bbdedfdb4
NthPortal <nthportal@gmail.com>
2017-01-25 21:07:40 -0500

    Add tests for CancellableTask

---
+++ src/test/scala/com/nthportal/concurrent/CancellableTaskTest.scala
@@ -0,0 +1,43 @@
+package com.nthportal.concurrent
+
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.concurrent.{Await, CancellationException}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+class CancellableTaskTest extends FlatSpec with Matchers {
+
+  behavior of "CancellableTask"
+
+  it should "contain the result of the task" in {
+    val t1 = CancellableTask {true}
+    Await.result(t1.future, Duration.Inf) should be (true)
+
+    val ex = new Exception
+    val t2 = CancellableTask {throw ex}
+    Await.result(t2.future.failed, Duration.Inf) should be theSameInstanceAs ex
+  }
+
+  it should "cancel" in {
+    val task = CancellableTask {Thread.sleep(5000)}
+    task.cancel(true) should be (true)
+    task.isCancelled should be(true)
+    Await.result(task.future.failed, Duration.Zero).isInstanceOf[CancellationException] should be (true)
+  }
+
+  it should "wrap CancellationExceptions" in {
+    val ex = new CancellationException("Not actually cancelled")
+
+    val t1 = CancellableTask {throw ex}
+    val res1 = Await.result(t1.future.failed, Duration.Inf)
+    res1.isInstanceOf[OtherCancellationException] should be (true)
+    res1.getCause should be theSameInstanceAs ex
+
+    val t2 = CancellableTask {throw new OtherCancellationException(ex)}
+    val res2 = Await.result(t2.future.failed, Duration.Inf)
+    res2.isInstanceOf[OtherCancellationException] should be (true)
+    res2.getCause.isInstanceOf[OtherCancellationException] should be (true)
+    res2.getCause.getCause should be theSameInstanceAs ex
+  }
+}
